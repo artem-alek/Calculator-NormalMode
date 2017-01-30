f42ae1328e9ac835de7b9c5b38ef8b7e071c13dd
Artem Aleksandruk <artem.alek@icloud.com>
2017-01-30 15:54:02 -0500

    working calculator with two numbers for only addition.

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-consol.log("This is a test");
+
+var a = document.getElementById("value-one");
+var b = document.getElementById("value-two");
+var button = document.getElementById("calculate-button");
+var calculationDiv = document.getElementById("answer");
+
+function calculate () {
+    var finalcalc = Number(a.value) + Number(b.value);
+    console.log(finalcalc);
+    calculationDiv.innerHTML = finalcalc;
+}
+
+button.addEventListener('click', calculate);
